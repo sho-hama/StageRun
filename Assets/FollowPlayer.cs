6986996683de26d9f27797fbf3709878cfdf04f0
sho-hama <shoki-hamana@kwansei.ac.jp>
2019-02-18 20:38:06 +0900

    Add original playable object.

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target; //ターゲットへの参照
+    private Vector3 offset; //相対座標
+    void Start()
+    {
+        //カメラ自身と対象物との相対距離を求める
+        offset = GetComponent<Transform>().position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().position = target.position + offset;
+    }
+}
---
+++ Assets/MoveSphere.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour {
+    float speed = 10;
+    float jump_power = 300;
+    bool spaceKeyDown;
+    static int MaxJumpTime = 2;
+    int JumpAvail = MaxJumpTime;
+    // Update is called once per frame
+  
+    void FixedUpdate() {
+      
+        float x = Input.GetAxis("Horizontal");
+        float y = 0.0f;
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        if (spaceKeyDown==true && JumpAvail>0){
+            y = 1.0f;
+            JumpAvail -= 1;
+       
+        }
+        rigidbody.velocity *= 0.95f;//摩擦による減速 
+        rigidbody.AddForce(x * speed, y * jump_power, z * speed);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){ spaceKeyDown = true; }
+        else { spaceKeyDown = false; }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        JumpAvail = MaxJumpTime;
+    }
+}
